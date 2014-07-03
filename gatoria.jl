a6cbf6397fe449a7887835ba1ce5b59a3db3b285
Brandon Bloom <brandon@brandonbloom.name>
2014-07-02 21:04:31 -0400

    intervals

--- gatoria.jl
+++ gatoria.jl
@@ -47,9 +47,12 @@ function join_content(content::Value, increment::Value)
 end
 
 join_content(::Type{None}, ::Type{None}) = None
-join_content(::Type{None}, v::Value) = v
-join_content(v::Value, ::Type{None}) = v
-join_content(x, y) = join_content(promote(to_content(x), to_content(y))...)
+join_content(::Type{None}, x) = to_content(x)
+join_content(x, ::Type{None}) = to_content(x)
+
+function join_content(x, y)
+    join_content(promote(to_content(x), to_content(y))...)
+end
 
 function add_content(cell, content)
     new_content = join_content(cell.content, content)
@@ -81,8 +84,8 @@ function lift(f::Function)
     function(cells...)
         inputs = cells[1:end-1]
         output = last(cells)
-        propagator(inputs, () -> add_content(output,
-                                             f(map(content, inputs)...)))
+        propagator(inputs, () ->
+          add_content(output, f(map(content, inputs)...)))
     end
 end
 
@@ -96,7 +99,7 @@ function handles_none(f)
     end
 end
 
-function run()
+function run() #TODO: do automatically when getting content; don't re-enter
     while !isempty(network.active)
         p = pop!(network.active)
         p.thunk()
@@ -109,6 +112,7 @@ adder = lift(handles_none(+))
 subtractor = lift(handles_none(-))
 multiplier = lift(handles_none(*))
 divider = lift(handles_none(/))
+exponenter = lift(handles_none(^))
 
 function sum(x, y, total)
     adder(x, y, total)
@@ -120,6 +124,14 @@ function product(x, y, total)
     multiplier(x, y, total)
     divider(total, x, y)
     divider(total, y, x)
+end
+
+squarer(x, total) = exponenter(x, cell(2), total)
+sqrter(x, total) = exponenter(x, cell(1/2), total)
+
+function quadratic(x, sq)
+    squarer(x, sq)
+    sqrter(sq, x)
 end
 
 ##########
@@ -157,21 +169,64 @@ println("k: ", content(k))
 ###########
 
 immutable Interval{N<:Number}
-    min::N
-    max::N
+    low::N
+    high::N
 end
 
-function intersect(a::Interval, b::Interval)
-    i = Interval(max(a.min, b.min), min(a.max, b.max))
-    i.min > i.max ? None : i
+function intersect(x::Interval, y::Interval)
+    i = Interval(max(x.low, y.low), min(x.high, y.high))
+    i.low > i.high ? None : i
 end
 
-to_content(i::Interval) = i
-from_content(i::Interval) = i
+==(x::Interval, y::Interval) = x.low == y.low && x.high == y.high
++(x::Interval, y::Interval) = Interval(x.low + y.low, x.high + y.high)
+-(x::Interval, y::Interval) = Interval(x.low - y.low, x.high - y.high)
+
+function *(x::Interval, y::Interval)
+    p1 = x.low * y.low
+    p2 = x.low * y.high
+    p3 = x.high * y.low
+    p4 = x.high * y.high
+    Interval(min(p1, p2, p3, p4), max(p1, p2, p3, p4))
+end
+
+function /(x::Interval, y::Interval)
+    if y.low <= 0 && 0 <= y.high
+        None #XXX
+    else
+        x * Interval(1 / x.high, 1 / x.low)
+    end
+end
+
+^(x::Interval, i::Integer) = Interval(x.low ^ i, x.high ^ i)
 
 #XXX handle promotion of N; eg Interval{Int64} and Int32
 promote_rule{N<:Number}(I::Type{Interval{N}}, ::Type{N}) = I
 convert{N<:Number}(I::Type{Interval{N}}, n::Number) = I(n, n)
-join_content{N<:Number}(a::Interval{N}, b::Interval{N}) = intersect(a, b)
+
+to_content(i::Interval) = i
+from_content(i::Interval) = i
+join_content{N<:Number}(x::Interval{N}, y::Interval{N}) = intersect(x, y)
 
 #########
+
+function fall_duration(t, h)
+    g = cell(Interval(9.789, 9.832))
+    half = cell(Interval(1/2, 1/2)) #TODO try just 0.5
+    t_sq = cell()
+    gt_sq = cell()
+    quadratic(t, t_sq)
+    product(g, t_sq, gt_sq)
+    product(half, gt_sq, h)
+end
+
+fall_time = cell()
+building_height = cell()
+fall_duration(fall_time, building_height)
+
+add_content(fall_time, Interval(2.9, 3.1))
+
+run()
+
+println("fall_time: ", content(fall_time))
+println("building_height: ", content(building_height))
